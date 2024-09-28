439d81379d823db2f778094b1e62d7c88dadc1ea
benrey <ben.rey@gmail.com>
2024-09-28 15:20:12 -0500

    smallchanges

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
-import buttonGradient from "./assets/svg/ButtonGradient";
-const App = () => {
-  return (
-    <>
-      <h1 className="text-3xl font-bold underline">Hello world!!</h1>;
-      <buttonGradient />
-    </>
-  );
-};
+import ButtonGradient from "./assets/svg/ButtonGradient";
+const App = () => (
+  <>
+    <h1 className="text-3xl font-bold underline">Hello world!!</h1>
+    <ButtonGradient />
+  </>
+);
 
 export default App;
